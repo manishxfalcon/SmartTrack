3aaf3dfb15a8c66578d50e8bdc07c03791569982
Manish Roy <manishxfalcon@gmail.com>
2024-03-08 21:17:51 +0530

    Deployment Configuration for Azure

--- application.py
+++ application.py
@@ -1,26 +1,29 @@
-from flask import Flask,request,render_template
+# Import necessary modules and functions
+from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 
+# Import preprocessing and prediction pipeline
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-application=Flask(__name__)
+# Create Flask application instance
+application = Flask(__name__)
+app = application
 
-app=application
-
-## Route for a home page
-
+# Route for the home page
 @app.route('/')
 def index():
+    # Render the index.html template
     return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
+# Route for prediction data
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
-    else:
-        data=CustomData(
+    # Check if the request method is POST
+    if request.method == 'POST':
+        # Collect form data
+        data = CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
@@ -28,19 +31,19 @@ def predict_datapoint():
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('writing_score')),
             writing_score=float(request.form.get('reading_score'))
-
         )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
+        # Convert data to DataFrame
+        pred_df = data.get_data_as_data_frame()
+        # Initialize prediction pipeline
+        predict_pipeline = PredictPipeline()
+        # Make prediction
+        results = predict_pipeline.predict(pred_df)
+        # Render index.html with prediction results
+        return render_template('index.html', results=results[0])
+    else:
+        # Render index.html without any data
+        return render_template('index.html')
 
-        predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
-    
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0")        
-
+# Run the Flask application
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
